9bba4931dc44984e18ca0cdff93aa7f38bc4e44f
Carlos Rangel <charlesleimaginant@gmail.com>
2023-12-25 22:13:25 -0600

    Added ReactJS to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://frabjous-lolly-6837d2.netlify.app/"
+  site: "https://frabjous-lolly-6837d2.netlify.app/",
+  integrations: [react()]
 });
