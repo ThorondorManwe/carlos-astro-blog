5527f9a7b8e83a4a2cbcc494f288da8ecfaafc1f
Carlos Rangel <charlesleimaginant@gmail.com>
2023-12-25 19:44:23 -0600

    Add code for a RSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://frabjous-lolly-6837d2.netlify.app/"
+});
